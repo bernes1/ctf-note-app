773d9f9be2310fc241f0cf71a2f7c66c5aa0ed58
bernes1 <37117219+bernes1@users.noreply.github.com>
2024-05-16 07:51:13 +0200

    chore: Add import functionality for DJ sets from JSON file

--- src/main.go
+++ src/main.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 
@@ -52,8 +53,10 @@ func main() {
 	defer pool.Close()
 
 	fmt.Println("Startup successful")
+
+	// Get user input
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Enter 'add' to add a new DJ set, 'export' to export all DJ sets, or 'all' to list all DJ sets")
+	fmt.Println("Enter 'add' to add a new DJ set, 'export' to export all DJ sets, 'import' to import DJ sets, or 'all' to list all DJ sets")
 	input, _ := reader.ReadString('\n')
 	input = strings.TrimSpace(input)
 
@@ -63,6 +66,8 @@ func main() {
 		exportDjSets(pool)
 	} else if input == "all" {
 		getAllDJSets(pool)
+	} else if input == "import" {
+		handleImportDJsets(pool)
 	} else {
 		fmt.Println("Invalid input")
 		os.Exit(1)
@@ -97,7 +102,9 @@ func getAllDJSets(db *pgxpool.Pool) {
 	pgxscan.Select(ctx, db, &djsets, `SELECT djset.*, platform.name as platform_name, artist.name as artist_name FROM djset 
                                       JOIN platform ON djset.platform_id = platform.id
                                       JOIN artist ON djset.artist_id = artist.id`)
-
+	sort.Slice(djsets, func(i, j int) bool {
+		return djsets[i].Id < djsets[j].Id
+	})
 	for _, djset := range djsets {
 		fmt.Printf("ID: %d, Name: %s, URL: %s, Platform: %s, Artist: %s\n", djset.Id, djset.Name, djset.Url, djset.PlatformName, djset.ArtistName)
 	}
@@ -113,6 +120,7 @@ func addNewArtist(db *pgxpool.Pool, artistName string) (int, error) {
 	}
 	return artistID, nil
 }
+
 func addNewPlatform(db *pgxpool.Pool, platformName string) (int, error) {
 	var platformID int
 	err := db.QueryRow(context.Background(), "INSERT INTO platform (name) VALUES ($1) RETURNING id", platformName).Scan(&platformID)
@@ -156,10 +164,15 @@ func addNewDJSet(db *pgxpool.Pool) error {
 		// Add the new artist to the database and get the ID
 		artistID, err = addNewArtist(db, artistName)
 		if err != nil {
+			fmt.Println("Error adding new artist:", err)
 			return err
 		}
 	} else {
-		artistID, _ = strconv.Atoi(artistInput)
+		artistID, err = strconv.Atoi(artistInput)
+		if err != nil {
+			fmt.Println("Invalid artist ID")
+			return err
+		}
 	}
 
 	// Do the same for the platform
@@ -177,10 +190,15 @@ func addNewDJSet(db *pgxpool.Pool) error {
 		// Add the new platform to the database and get the ID
 		platformID, err = addNewPlatform(db, platformName)
 		if err != nil {
+			fmt.Println("Error adding new artist:", err)
 			return err
 		}
 	} else {
-		platformID, _ = strconv.Atoi(platformInput)
+		platformID, err = strconv.Atoi(platformInput)
+		if err != nil {
+			fmt.Println("Invalid platform ID")
+			return err
+		}
 	}
 
 	// Get the DJ set details
@@ -202,6 +220,33 @@ func addNewDJSet(db *pgxpool.Pool) error {
 
 }
 
+// import function
+func importDJsets(db *pgxpool.Pool, filePath string) {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		fmt.Printf("unable to read file due to %s\n", err)
+	}
+	var djsets []DJset
+	err = json.Unmarshal(data, &djsets)
+	if err != nil {
+		fmt.Printf("unable to unmarshal JSON due to %s\n", err)
+		return
+	}
+	for _, djset := range djsets {
+		err = addDJSet(db, int(djset.ArtistId), int(djset.PlatformId), djset.Name, djset.Url)
+		if err != nil {
+			fmt.Printf("unable to add DJ set due to %s\n", err)
+		}
+	}
+}
+func handleImportDJsets(db *pgxpool.Pool) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the file path for your .json to import DJ sets")
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+	importDJsets(db, input)
+}
+
 // json export function
 func exportDjSets(db *pgxpool.Pool) {
 	ctx := context.Background()
@@ -213,12 +258,18 @@ func exportDjSets(db *pgxpool.Pool) {
 	djsetList := make([]*DJset, 0)
 	for _, djset := range djsets {
 		djsetList = append(djsetList, djset)
-		fmt.Println(djset)
 	}
+
+	sort.Slice(djsetList, func(i, j int) bool {
+		return djsetList[i].Id < djsetList[j].Id
+	})
+
 	jsonexport, err := json.MarshalIndent(djsetList, "", "  ")
 	if err != nil {
 		fmt.Println("Error marshalling json:", err)
 	}
+	fmt.Println(string(jsonexport))
+
 	err = os.WriteFile("djsetexport.json", jsonexport, os.ModePerm)
 	if err != nil {
 		fmt.Println("Error writing json file:", err)
